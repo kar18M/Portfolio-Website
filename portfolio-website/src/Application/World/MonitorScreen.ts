1f9f4d87ad100ca99f55c6c9c32abf9bd3d00e90
karthick <km606462@gmail.com>
2026-04-19 21:01:03 +0530

    feat: Add SEO meta tags, sitemap, and robots.txt

--- portfolio-website/src/Application/World/MonitorScreen.ts
+++ portfolio-website/src/Application/World/MonitorScreen.ts
@@ -184,7 +184,7 @@ export default class MonitorScreen extends EventEmitter {
 
         // Set iframe attributes
         // PROD
-        iframe.src = 'http://localhost:3000/';
+        iframe.src = 'https://portfolio-inner-site-two.vercel.app/';
         /**
          * Use dev server is query params are present
          *
